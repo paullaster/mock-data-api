943c195ba49d7fb6a1f2556bc374e9c16ad69d66
paullaster <paullasterokoth98@gmail.com>
2022-11-02 00:20:26 +0300

    update users

--- src/fakedb/fakedb.js
+++ src/fakedb/fakedb.js
@@ -12,6 +12,97 @@ let users = {
         id: uuidv4 (),
         firstname: 'Paullaster',
         lastname: 'Okoth',
+        isDeveloper: true,
+    },
+    [ uuidv4 ()]: {
+        id: uuidv4 (),
+        firstname: 'Arthur',
+        lastname: 'Oduor',
         isDeveloper: false,
     },
 }
+
+//Fake endpoint:
+//reading all users:
+const getUsers = () => {
+    return new Promise ( (resolve, reject) => {
+        if (!users) {
+            return setTimeout (
+                () => reject (new Error ('User not found')), 250);
+        };
+        return setTimeout (
+           () => resolve ( Object.values (users) ), 250
+        );
+    })
+}
+
+// reading single user:
+const getUser = (id) => {
+    return new Promise ( (resolve, reject) => {
+        const user = users[id];
+        if (!user) {
+            return setTimeout (
+                () => reject ( new Error ( 'User not found')), 250
+            )
+        };
+        return setTimeout (
+            () => resolve (users[id]), 250
+        )
+    });
+};
+
+// Creating a new user
+const createUser = ( data) => {
+    return new Promise ( (resolve, reject) => {
+        if ( !data.firstname || !data.lastname ) {
+            return setTimeout (
+                () => reject ( new Error ( 'Cannot create user, provide all information!') ), 250
+            );
+        };
+        const newUser = {
+            id: uuidv4 (),
+            ...data
+        }
+        users = {
+            ...users,
+            [uuidv4 ()]: newUser
+        };
+        return setTimeout (
+
+            () => resolve (true), 250
+        );
+    });
+};
+
+//Updating an existing user
+const updateUser = ( id, data) => {
+    return new Promise ( (resolve, reject) => {
+        const user = users[id];
+        if (!user) {
+            return setTimeout (
+                () => reject ( new Error ( 'User not found') ), 250
+            );
+        };
+        users[id] = {
+            ...users[id],
+            ...data
+        };
+        return setTimeout (
+            () => resolve (true), 250
+        );
+    });
+};
+
+// Deleting an existing user
+const deleteUser = ( id) => {
+    return new Promise ( (resolve, reject) => {
+        const {[id]:user, ...rest} = users;
+
+        if ( !user) {
+            return setTimeout (
+                () => reject ( new Error ( 'user not found!')), 250
+            );
+        }
+
+    });
+};
